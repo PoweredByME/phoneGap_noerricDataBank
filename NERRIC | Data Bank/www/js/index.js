ea31ddb31144020f534d75b7b4e55644c70c8f69
PoweredByME <saadkwi12@hotmail.com>
2017-11-10 16:33:42 +0500

    Test

--- NERRIC | Data Bank/www/js/index.js
+++ NERRIC | Data Bank/www/js/index.js
@@ -32,11 +32,10 @@ function getImage(){
 
 
 function success(imageData) {
-    
-    
     $(".b64p").empty();
     $(".b64p").append("image data");
-    $(".b64p").append(imageData);
+    $(".b64p").append("data " + imageData);
+    document.getElementById("image").src = "data:image/jpeg;base64," + imageData;
 }
 
 function fail(error) { 
